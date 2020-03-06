519855866fd9ec74da0507445b37d576bb85a339
Steve Hill <hello@stevenhill.me>
2020-03-06 01:22:21 +0000

    Add Navigation data and make for loop

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,7 @@
+// Import data files
+const site = require('./src/_data/site.json');
+const navigation = require('./src/_data/navigation.json');
+
 module.exports = function(config) {
   return {
     dir: {
