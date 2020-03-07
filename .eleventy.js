c41f644b649e182300dcf953f3a271dd2b817e3c
Steve Hill <hello@stevenhill.me>
2020-03-07 19:20:57 +0000

    Add SASS processing

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,11 @@
 // Import data files
 const site = require('./src/_data/site.json');
 const navigation = require('./src/_data/navigation.json');
+const sass = require('./src/_config/sass-process.js');
 
-module.exports = function(config) {
+module.exports = config => {
+  //Watching for modificaions in style directory
+  sass('./src/assets/scss/main.scss', './dist/assets/css/main.css');
   return {
     dir: {
       input: 'src',
