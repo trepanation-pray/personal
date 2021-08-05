3734d1add30a50ec8126ca8fd43f48d1376d1b4c
Steve Hill <hello@stevenhill.me>
2021-08-05 22:36:02 +0100

    Random colour and background image on page load

--- src/assets/scripts/common.js
+++ src/assets/scripts/common.js
@@ -8,3 +8,8 @@ document.body.addEventListener('click', function (event) {
   document.body.classList.toggle('menu-active');
 
 }, false);
+
+document.body.querySelector('.o-hero').style.backgroundImage = 
+'url("assets/images/marble-ink-0' + (Math.floor(Math.random() * 5) + 1) + '.jpg"';
+
+document.body.querySelector('.o-hero').classList.add('o-hero--'+ (Math.floor(Math.random() * 5) + 1))
