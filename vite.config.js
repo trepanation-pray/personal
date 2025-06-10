a33ee92b1dfa515a831640a3d77348695179ef6f
Steven Hill <hello@stevenhill.me>
2025-06-10 22:35:38 +0100

    Refine Vite integration

--- vite.config.js
+++ vite.config.js
@@ -8,13 +8,8 @@ export default {
         main: resolve(__dirname, 'src/assets/main.js')
       },
       output: {
-        entryFileNames: 'assets/[name].js',
-        assetFileNames: (info) => {
-          if (info.name && info.name.endsWith('.css')) {
-            return 'assets/[name][extname]';
-          }
-          return 'assets/[name][extname]';
-        }
+        entryFileNames: 'assets/[name].[hash].js',
+        assetFileNames: 'assets/[name].[hash][extname]'
       }
     }
   }
